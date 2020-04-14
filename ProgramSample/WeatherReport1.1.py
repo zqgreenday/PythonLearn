df6f0aee70a49a41711a4eb4d9ad27f49c9b1e5d
天涯浪人 <61607346+zqgreenday@users.noreply.github.com>
2020-04-14 16:20:07 +0800

    Add files via upload

--- ProgramSample/WeatherReport1.1.py
+++ ProgramSample/WeatherReport1.1.py
@@ -6,13 +6,17 @@ import tkinter.messagebox
 import time
 
 # 爬取天气预报信息
-def get_city_everyday_weather():
-    url = 'http://wthrcdn.etouch.cn/weather_mini?city=淄博'  # 淄博天气预报地址
-    r = requests.get(url)
-    all = json.loads(r.text)  # 获取到json格式的内容，内容很多
-    # print(all)  # json内容，通过这行代码来确定每日一句的键名
-    data_all = all['data']
-    return data_all  # 返回结果
+def get_city_everyday_weather(city_name):
+    try:
+        url = 'http://wthrcdn.etouch.cn/weather_mini?city='+city_name  # 淄博天气预报地址
+        r = requests.get(url)
+        all = json.loads(r.text)  # 获取到json格式的内容，内容很多
+        # print(all)  # json内容，通过这行代码来确定每日一句的键名
+        data_all = all['data']
+        return data_all  # 返回结果
+    except:
+        tkinter.messagebox.showerror(title='Error', message='没有查到' + city_name + '的天气数据，请检查城市名称')
+
 
 
 # 爬取爱词霸每日鸡汤
@@ -31,30 +35,58 @@ class MyApplication(tk.Tk):
         self.resizable(width=False, height=False)
         self.configure(background='#77eb34')
         self.localdate = time.strftime("%Y-%m-%d", time.localtime())
-        self.weatherdata = get_city_everyday_weather()
+        self.appUI()
+        try:
+            self.weatherdata()
+            self.everydaysoup()
+        except Exception as e:
+            self.messagebox.showerror(title='Error', message=str(e))
 
+
+    def appUI(self):
         self.title_lable = ttk.Label(self, text="天 气 预 报", font=("微软雅黑", 30), relief='ridge',
-                                anchor='center', background='#34bdeb')
+                                     anchor='center', background='#34bdeb')
         self.title_lable.place(x=150, y=5, width=500)
-        self.city_lable= ttk.Label(self, text="城市："+self.weatherdata['city'], font=("微软雅黑", 12),
-                              anchor='center', background='#77eb34')
-        self.city_lable.place(x=15, y=20)
+        self.name_lable = ttk.Label(self, text="城市：", font=("微软雅黑", 12), anchor='center', background='#77eb34')
+        self.name_lable.place(x=5, y=5)
+        self.city_entry = ttk.Entry(self, width=9, font=("微软雅黑", 12))
+        self.city_entry.insert(0, '淄博')
+        self.city_entry.place(x=55, y=5)
+        self.city_button = ttk.Button(self, text='确定', width=10, command=self.weatherdata)
+        self.city_button.place(x=60, y=42)
+        self.city_lable= ttk.Label(self, text="城市：淄博", font=("微软雅黑", 12),
+                                   anchor='center', background='#77eb34')
+        self.city_lable.place(x=655, y=5)
         self.date_lable = ttk.Label(self, text="日期："+self.localdate, font=("微软雅黑", 11),
-                               anchor='center', background='#77eb34')
-        self.date_lable.place(x=655, y=20, width=135)
+                                    anchor='center', background='#77eb34')
+        self.date_lable.place(x=655, y=30, width=135)
 
+    def dataupdate(self, city_entry):
+        name = city_entry.get().strip()
+        weatherdata = get_city_everyday_weather(name)
+        return weatherdata
+
+    def weatherdata(self):
+        self.weatherdata = self.dataupdate(self.city_entry)
+        self.city_lable['text'] = "城市："+self.weatherdata['city']
         RealTimeWeather(self, text="实时温度", labelanchor="nw", font=("微软雅黑", 15),
                         background='#34ebb4').place(x=4, y=80, height=200, width=260)
+
         TodayWeather(self, text="今日天气", labelanchor="nw", font=("微软雅黑", 15),
                      background='#34ebb4').place(x=268, y=80, height=200, width=260)
+
         TomorrowWeather(self, text="明日天气", labelanchor="nw", font=("微软雅黑", 15),
                         background='#34ebb4').place(x=532, y=80, height=200, width=260)
+
+    def everydaysoup(self):
         EveryDaySoup(self, text="每日鸡汤", labelanchor="nw", font=("微软雅黑", 15),
                     background='#ebc334').place(x=4, y=290, height=200, width=790)
 
 class RealTimeWeather(tk.LabelFrame):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
+        for widget in self.winfo_children():
+            widget.destroy()
         self.wendu = parent.weatherdata['wendu']
         #self.shidu = self.weatherdata['shidu']
         #self.pm25 = self.weatherdata['pm25']
@@ -84,6 +116,8 @@ class RealTimeWeather(tk.LabelFrame):
 class TodayWeather(tk.LabelFrame):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
+        for widget in self.winfo_children():
+            widget.destroy()
         self.forecast = parent.weatherdata['forecast']
         self.todayweatherdata = self.forecast[0]
         self.high = self.todayweatherdata['high']
@@ -115,6 +149,8 @@ class TodayWeather(tk.LabelFrame):
 class TomorrowWeather(tk.LabelFrame):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
+        for widget in self.winfo_children():
+            widget.destroy()
         self.forecast = parent.weatherdata['forecast']
         self.tomweatherdata = self.forecast[1]
         self.high = self.tomweatherdata['high']
@@ -150,13 +186,14 @@ class EveryDaySoup(tk.LabelFrame):
         self.English = self.soupdata['content']  # 提取json中的英文鸡汤
         self.Chinese = self.soupdata['note']  # 提取json中的中文鸡汤
 
-        self.eng_label = ttk.Label(self, text=self.English, font=("微软雅黑", 20),
+        self.eng_label = ttk.Label(self, text=self.English, font=("Arial", 20),
                               background='#ebc334', wraplength=750)
         self.eng_label.grid(row=0, column=0, sticky='w')
-        self.chn_label = ttk.Label(self, text=self.Chinese, font=("微软雅黑", 20),
+        self.chn_label = ttk.Label(self, text=self.Chinese, font=("方正舒体", 20),
                               background='#ebc334', wraplength=750)
         self.chn_label.grid(row=1, column=0, sticky='w')
 
 if __name__ == '__main__':
     app = MyApplication()
+    app.update()
     app.mainloop()
